d4c910b2ddb4f2493dbd5e8bd1851c14edb9cbe2
Adam Drakeford <adamdrakeford@gmail.com>
2017-11-16 12:28:21 +0000

    Implemented go server

--- grpctest/go/main.go
+++ grpctest/go/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/dr4ke616/grpc-test/client"
+	"github.com/dr4ke616/grpc-test/server"
 )
 
 func main() {
 	s, _ := client.Test()
 	fmt.Println("hello world ", s)
+
+	server.Start(8021)
 }
